6be601f10325f9a30fd721094c32f615ffe755b2
troyano315 <luciano315@hotmail.com>
2022-07-27 22:16:05 -0400

    Updated shuttle requirements

--- script.js
+++ script.js
@@ -6,34 +6,57 @@ window.addEventListener("load", function(){
    let copilotName = document.getElementById("copilotName");
    let fuelLevel = document.getElementById("fuelLevel");
    let cargoMass = document.getElementById("cargoMass");
-      pilotName.value
-
+   let itemStatusNode = document.getElementById("itemStatus")
+   let launchStatusNode = document.getElementById("launchStatus")
    formNode.addEventListener("submit", function(event){
     event.preventDefault();   
 
-       if (pilotName.value.length === 0) {
+       if (pilotName.value.length === 0 || !isNaN(pilotName.value)) {
          //event.preventDefault();
-           alert("Please fill out all boxes")
-       } else if (copilotName.value.length === 0){
+           alert("Please enter a word for Pilot Name  ")
+       } else if (copilotName.value.length === 0 || !isNaN(copilotName.value)){
          //event.preventDefault();
-         alert("Please fill out all boxes")
-       } else if (isNaN(fuelLevel.value) || fuelLevel.value.length === 0 || fuelLevel.value < 10000){
+         alert("Please enter a word for CoPilot Name ")
+       } else if (isNaN(fuelLevel.value) || fuelLevel.value.length === 0){
          //event.preventDefault();
-         alert("Please enter a number over 10000 for fuelLevel")
+         alert("Please enter a number for fuelLevel")
        } else if (isNaN(cargoMass.value) || cargoMass.value.length === 0){
          //event.preventDefault();
          alert("Please enter a number for cargoMass")
        };
 
-       let itemStatusNode = document.getElementById("itemStatus")
-   
-itemStatusNode.innerHTML = `
-<li id="pilotStatus">${pilotName.value}</li>
-<li id="copilotStatus">${copilotName.value}</li>
-<li id="fuelStatus">${fuelLevel.value}</li>
-<li id="cargoStatus">${cargoMass.value}</li>
-`;
-   console.log(pilotName.value)
+       if (fuelLevel.value < 10000) {
+         launchStatusNode.innerHTML = `
+         <h2 id="launchStatus" style="color:red">"Shuttle not ready for launch"</h2>
+         `;
+         itemStatusNode.innerHTML = `
+         <li id="pilotStatus">${pilotName.value} is not ready for launch</li>
+         <li id="copilotStatus">${copilotName.value} is not ready for launch</li>
+         <li id="fuelStatus">Fuel level check failed. Please fill fuel over 10000 </li>
+         <li id="cargoStatus">Cargo Mass: ${cargoMass.value} </li>
+         `;
+       } else {
+         launchStatusNode.innerHTML = `
+         <h2 id="launchStatus" style="color:green">"Shuttle is ready for launch"</h2>
+         `;
+         itemStatusNode.innerHTML = `
+         <li id="pilotStatus">${pilotName.value} is ready for launch</li>
+         <li id="copilotStatus">${copilotName.value} is ready for launch</li>
+         <li id="fuelStatus">Fuel level check passed</li>
+         <li id="cargoStatus">Cargo Mass: ${cargoMass.value}</li>
+         `;
+       }
+
+       if (cargoMass.value > 10000) {
+         launchStatusNode.innerHTML = `
+         <h2 id="launchStatus" style="color:red">"Shuttle not ready for launch"</h2>
+         `;
+         itemStatusNode.innerHTML = `
+         <li id="pilotStatus">${pilotName.value} is not ready for launch</li>
+         <li id="copilotStatus">${copilotName.value} is not ready for launch</li>
+         <li id="cargoStatus">There is too much mass for the shuttle to take off.</li>
+         `;
+       }
 
    });
 
